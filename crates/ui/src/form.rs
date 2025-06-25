819161ec586a4f7dd1f99b56758044ecbd8520e8
Jason Lee <huacnlee@gmail.com>
2025-06-25 22:30:09 +0800

    form: Fix form label required not in same line. (#1013)
    ![image](https://github.com/user-attachments/assets/2435e099-4d92-4a52-a825-d7b23e27e5d1)

--- crates/ui/src/form.rs
+++ crates/ui/src/form.rs
@@ -377,13 +377,15 @@ impl RenderOnce for FormField {
                                 .gap_1()
                                 .items_center()
                                 .when_some(self.label, |this, builder| {
-                                    this.child(builder.render(window, cx)).when(
-                                        self.required,
-                                        |this| {
-                                            this.child(
-                                                div().text_color(cx.theme().danger).child("*"),
-                                            )
-                                        },
+                                    this.child(
+                                        h_flex().gap_1().child(builder.render(window, cx)).when(
+                                            self.required,
+                                            |this| {
+                                                this.child(
+                                                    div().text_color(cx.theme().danger).child("*"),
+                                                )
+                                            },
+                                        ),
                                     )
                                 }),
                         )
